0bbe1a759ad701334f490fbe6f8e5a98c6efdef6
MBOUP MOUHAMADOU LAMINE p1922789 <mouhamadou-lamine.mboup@etu.univ-lyon1.fr>
2021-07-12 21:30:02 +0200

    ajout

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Components/Header';
+import SwipeButtons from './Components/SwipeButtons';
+import TinderCards from './Components/TinderCards';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      <TinderCards />
+      <SwipeButtons />
+
+    </div>
+  );
+}
+
+export default App;
